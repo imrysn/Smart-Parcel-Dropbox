32424c52d88b93a595fd7f53a5b19321f5a360ad
yadime <brantcordova0@gmail.com>
2025-12-13 12:56:55 +0800

    Merge branch 'Brant' of https://github.com/imrysn/Smart-Parcel-Dropbox into Brant

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /// Model for user data
 class UserModel {
   final String uid;
@@ -45,51 +44,3 @@ class UserModel {
     };
   }
 }
-=======
-/// Model for user data
-class UserModel {
-  final String uid;
-  final String email;
-  final String fullName;
-  final String phoneNumber;
-  final String address;
-  final String role; // user or courier
-  final DateTime? createdAt;
-
-  UserModel({
-    required this.uid,
-    required this.email,
-    required this.fullName,
-    required this.phoneNumber,
-    required this.address,
-    required this.role,
-    this.createdAt,
-  });
-
-  /// Create UserModel from Map
-  factory UserModel.fromMap(Map<String, dynamic> data) {
-    return UserModel(
-      uid: data['uid'] ?? '',
-      email: data['email'] ?? '',
-      fullName: data['fullName'] ?? '',
-      phoneNumber: data['phoneNumber'] ?? '',
-      address: data['address'] ?? '',
-      role: data['role'] ?? 'user',
-      createdAt: data['createdAt']?.toDate(),
-    );
-  }
-
-  /// Convert to Map
-  Map<String, dynamic> toMap() {
-    return {
-      'uid': uid,
-      'email': email,
-      'fullName': fullName,
-      'phoneNumber': phoneNumber,
-      'address': address,
-      'role': role,
-      'createdAt': createdAt,
-    };
-  }
-}
->>>>>>> 188bd5ffd30b49c825dcafe3212dd5d0358ce1ac
